3e34f9976de6916bfc470e122910cc71a07e8dc0
Fararame <maythanan.ladee66@gmail.com>
2025-12-01 21:12:20 +0700

    Resume

--- app/views.py
+++ app/views.py
@@ -8,7 +8,9 @@ from app import app
 def home():
     return "Frame says 'Hello world!'"
 
-
+@app.route('/lab02')
+def resume():
+    return app.send_static_file('lab02_resume.html')
 
 
 @app.route('/phonebook')
