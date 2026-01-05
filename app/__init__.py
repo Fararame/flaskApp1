40b47acfe1c50d33a29715ed31897f3f385b0af2
Fararame <maythanan.ladee66@gmail.com>
2026-01-05 14:05:13 +0700

    Submission for Lab05

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__, static_folder='static')
+app.url_map.strict_slashes = False
 
 app.jinja_options = app.jinja_options.copy()
 app.jinja_options.update({
@@ -10,5 +11,13 @@ app.jinja_options.update({
 
 
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = \
+    'a48ee502057f1ad0e821803427f8972437e953b157c1e62b'
+
+@app.before_request
+def remove_trailing_slash():
+   # Check if the path ends with a slash but is not the root "/"
+    if request.path != '/' and request.path.endswith('/'):
+        return redirect(request.path[:-1], code=308)
 
 from app import views # noqa
